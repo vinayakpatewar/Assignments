348ab09900c90e809e4e2f5ba1ac991a84ba526d
Vinayak.patewar <patewarvinayak1@gmail.com>
2025-01-25 00:04:27 +0530

    Assignment 1 program

--- src/com/assignments/Assignments1.java
+++ src/com/assignments/Assignments1.java
@@ -1,10 +1,84 @@
 package com.assignments;
 
+import java.awt.geom.Arc2D.Float;
+
 public class Assignments1 {
 
 	public static void main(String[] args) {
 		String name ="Vinayak";
-
-	}
-
+		
+		System.out.println(name);
+		
+		float temperature = 25.5f;
+		System.out.println(temperature);
+		
+		double city_temperature = 25.5;
+		System.out.println(city_temperature);
+		
+		boolean order_placed=true;
+		System.out.println(order_placed);
+		
+		double phone_number=123-456-7890;
+		System.out.println(phone_number);
+		
+		double account_balance =1000.25;
+		System.out.println(account_balance);
+		
+		String email_address = "john.doe@example.com";
+		System.out.println(email_address);
+		
+		double latitude=37.7749;
+		System.out.println(latitude);
+		
+		double longitude=-122.4194;
+		System.out.println(longitude);
+		
+		boolean marital_status=true;
+		System.out.println(marital_status);
+		
+		String occupation="Software Engineer";
+		System.out.println(name);
+		
+		String favourite_color="Blue";
+		System.out.println(occupation);
+		
+		int year=2025;
+		System.out.println(year);
+		
+		double followers_on_social_media= 1000000;
+		System.out.println(followers_on_social_media);
+		
+		float movie_rating=7.5f;
+		System.out.println(movie_rating);
+		
+		double movie_rating1=7.5;
+		System.out.println(movie_rating1);
+		
+		char boold_group='A';
+		System.out.println(boold_group);
+		
+		String tital_of_group="To kill a Mockingbird";
+		System.out.println(tital_of_group);
+		
+		short no_of_employee=500;
+		System.out.println(no_of_employee);
+		
+		String event_time = "2:30 PM";
+		System.out.println(event_time);
+		
+		String country_name="India";
+		System.out.println(country_name);
+		
+		String Persons_eye_color="Brown";
+		System.out.println(Persons_eye_color);
+		
+		String persons_birthplace="Pune";
+		System.out.println(persons_birthplace);
+		
+		double city_distance=200.5;
+		System.out.println(city_distance);
+			
+			}
 }
+
+
